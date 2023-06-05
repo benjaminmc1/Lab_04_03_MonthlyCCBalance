574233724a8a57356a128ba9210fe5bf3dfebee9
Benjamin <bmckee566@gmail.com>
2023-06-05 13:36:54 -0400

    Task 3 finish

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        
+        double creditBalance = 5000;
+        double interestRate = .17;
+        double firstMonth;
+        double secondMonth;
+
+        firstMonth = creditBalance * interestRate;
+        System.out.println("The interest due for the first month is " + firstMonth + " dollars.");
+
+        creditBalance = creditBalance + firstMonth;
+        secondMonth = creditBalance * interestRate;
+        System.out.println("The interest due for the second month is " + secondMonth + " dollars.");
     }
 }
